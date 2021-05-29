ada1fdf1d3840b44236387374793cbaa17a381be
lin-125 <435332240@qq.com>
2021-05-29 15:16:57 +0800

    3

---
+++ js/myScript.js
@@ -0,0 +1,20 @@
+function msj(e) {
+    let j = document.getElementById(e).value;
+    if (!j) {
+        window.alert('!!!!!')
+        return;
+    }
+    var t1 = 'https://www.baidu.com/s?ie=UTF-8&wd=' + j;
+    open(t1, '_blank', '', false);
+}
+function tim(e) {
+    var t1, t2, r;
+    var d = new Date();
+    t1 = '<b>' + d.toLocaleDateString() + '</b>';
+    t2 = d.toLocaleTimeString();
+    r = t1 + '<br>' + t2;
+    document.getElementById(e).innerHTML = r;
+}
+function gob() {
+    window.scroll(0, 10);
+}
